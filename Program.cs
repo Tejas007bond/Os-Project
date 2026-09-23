87ceadf8f784f44f4ab5ffbf90102ba681936a87
Tejas007bond <tejasphalke762@gmail.com>
2026-09-23 21:31:12 +0530

    Initialize the directories and whitelist

--- Program.cs
+++ Program.cs
@@ -16,5 +16,22 @@ namespace UsbMonitorETW
         static string eventLogPath = Path.Combine(logDir, "usb_events.txt");
         static string alertLogPath = Path.Combine(logDir, "usb_alerts.log");
 
+        static void main(String[] args)
+        {
+            // 1. Ensuring running as administrator (reuires for ETW and Device Disabling)
+            if(!IsRunAsAdmin())
+            {
+                Console.WriteLine("[-] Error: This program must be run as Admistrator.");
+                return;
+            }
+
+            // 2. Initialize the directories and whitelist
+            Directory.CreateDirectory(logDir);
+            if (!File.Exists(whitelistPath))
+            {
+                File.WriteAllText(whitelistPath, "VID_80EE&PID_CAFE\n");
+                Console.WriteLine("[*] Created default whitelist.txt. Add allowed VID_PID combinations here.");
+            }
+        }
     }
 }
