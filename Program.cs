7401cb59dd30ff8a68d753b70ca27751eb6027b7
Tejas007bond <tejasphalke762@gmail.com>
2026-09-23 21:12:35 +0530

    Imported the libraries

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text.RegularExpressions;
+using Microsoft.Diagnostics.Tracing.Session;
+using Microsoft.Diagnostics.Tracing;
+
+namespace UsbMonitorETW
+{
+    
+}
